591ca8645e6c2c5e2ec7ca9de5a3d6b9268b53a4
fakhrilak <fakhrilak23@gmail.com>
2020-08-08 02:49:20 +0700

    Updates

---
+++ precache-manifest.445d8f3e99b85c51af331ec321849586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb65ed9f814a1de931bca855357d767e",
+    "url": "/downloaderyt/index.html"
+  },
+  {
+    "revision": "b7343d4994a2fb60d7db",
+    "url": "/downloaderyt/static/css/main.19f6988b.chunk.css"
+  },
+  {
+    "revision": "04da9fbda0bf4fff1ac5",
+    "url": "/downloaderyt/static/js/2.8247c7fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/downloaderyt/static/js/2.8247c7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7343d4994a2fb60d7db",
+    "url": "/downloaderyt/static/js/main.50f1c0c0.chunk.js"
+  },
+  {
+    "revision": "e695cfa036276a2e477b",
+    "url": "/downloaderyt/static/js/runtime-main.0b45b0c9.js"
+  }
+]);
